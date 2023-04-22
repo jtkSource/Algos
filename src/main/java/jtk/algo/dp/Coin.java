ff4a6a1b759e2508ea6d244c76d3768be6875671
Jubin Kuriakose <jubin.kuriakose@gmail.com>
2023-04-23 00:10:15 +0800

    Coin Counting

--- src/main/java/jtk/algo/dp/Coin.java
+++ src/main/java/jtk/algo/dp/Coin.java
@@ -4,9 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Set;
-import java.util.TreeSet;
 
 /**
  * You are given an integer array coins representing coins of different denominations and an integer amount
