fe48ad71b696569d446e72f4b1ee057f901e9be8
Jubin Kuriakose <jubin.kuriakose@gmail.com>
2023-04-23 18:22:54 +0800

    SlidingWindow

---
+++ src/main/java/jtk/algo/MaxSumSubArray.java
@@ -0,0 +1,29 @@
+package jtk.algo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Fixed Sliding Window
+ * Find the max sum subarray of a fixed size K
+ * Example input:
+ * [4,2,1,7,8,1,2,8,1,0]
+ */
+public class MaxSumSubArray {
+    private static final Logger log = LoggerFactory.getLogger(MaxSumSubArray.class);
+    public static int findMaxSumSubArray(int[] arr, int k){
+        int maxValue = Integer.MIN_VALUE;
+        int currentRunningSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            currentRunningSum += arr[i];
+            if(i >= (k-1)){
+              maxValue = Math.max(maxValue, currentRunningSum);
+              currentRunningSum -= arr[i-(k-1)] ;
+            }
+        }
+        return maxValue;
+    }
+    public static void main(String[] args) {
+        log.info("{}", findMaxSumSubArray(new int[]{4,2,1,7,8,1,2,8,1,0},3));
+    }
+}
